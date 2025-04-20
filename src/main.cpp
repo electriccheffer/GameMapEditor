3424a8a12d7d511428b827e603410f7f204e634d
electriccheffer <electriccheff@gmail.com>
2025-04-20 07:48:50 -0500

    REFACTORED WINDOW SIZE VARIABLES INTO A FUNCTION

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,44 @@
 #include "../include/window_functions.hpp"
 #include <iostream>
 
+
+	int detailsBoxWidth; 
+	int detailsBoxHeight;
+	int detailsBoxStartX; 
+	int detailsBoxStartY;
+	int mapEditorBoxWidth; 
+	int mapEditorBoxHeight;
+	int mapEditorBoxStartY;
+	int mapEditorBoxStartX;
+	int optionsBoxWidth; 
+	int optionsBoxHeight;
+	int optionsBoxStartY;
+	int optionsBoxStartX;
+
+void initializeWindowVariables(){
+
+	detailsBoxWidth = COLS; 
+	detailsBoxHeight = LINES / 2; 
+	detailsBoxStartX = 0; 
+	detailsBoxStartY = detailsBoxHeight; 
+	mapEditorBoxWidth = COLS/2; 
+	mapEditorBoxHeight = LINES / 2;
+	mapEditorBoxStartY = 0; 
+	mapEditorBoxStartX = 0;	
+	optionsBoxWidth = mapEditorBoxWidth; 
+	optionsBoxHeight = mapEditorBoxHeight; 
+	optionsBoxStartY = 0; 
+	optionsBoxStartX = COLS / 2;
+
+}
+
 int main(int argc, char** argv){
 
 	initscr(); 
 	cbreak();
         noecho();
 	curs_set(0);	
-	int detailsBoxWidth = COLS; 
-	int detailsBoxHeight = LINES / 2; 
-	int detailsBoxStartX = 0; 
-	int detailsBoxStartY = detailsBoxHeight; 
-	int mapEditorBoxWidth = COLS/2; 
-	int mapEditorBoxHeight = LINES / 2;
-	int mapEditorBoxStartY = 0; 
-	int mapEditorBoxStartX = 0;	
-	int optionsBoxWidth = mapEditorBoxWidth; 
-	int optionsBoxHeight = mapEditorBoxHeight; 
-	int optionsBoxStartY = 0; 
-	int optionsBoxStartX = COLS / 2; 
+	initializeWindowVariables(); 
 	WINDOW *detailsBoxWindow = create_new_window(detailsBoxHeight,detailsBoxWidth,detailsBoxStartY,detailsBoxStartX);
 	WINDOW *mapEditorBoxWindow = create_new_window(mapEditorBoxHeight,mapEditorBoxWidth,mapEditorBoxStartY,mapEditorBoxStartX);
 	WINDOW *optionsBoxWindow = create_new_window(optionsBoxHeight,optionsBoxWidth,optionsBoxStartY,optionsBoxStartX);
