7930d2095be67c3cd5151bde7d5b113034d2ac93
electriccheffer <electriccheff@gmail.com>
2025-04-20 07:43:51 -0500

    CREATED MULTI-WINDOWED APP

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 build : ./build/main.o
-	gcc ./build/main.o ./build/window_functions.o -o ./build/main -lncurses
+	g++ ./build/main.o ./build/window_functions.o -o ./build/main -lncurses
 	./build/main
 	rm ./build/main
 	rm ./build/window_functions.o ./build/main.o
 
 ./build/main.o: ./src/main.cpp ./build/window_functions.o 
-	gcc -c ./src/main.cpp -o ./build/main.o -lncurses
+	g++ -c ./src/main.cpp -o ./build/main.o -lncurses
 
 ./build/window_functions.o: ./lib/window_functions.cpp
-	gcc -c ./lib/window_functions.cpp -o ./build/window_functions.o 
+	g++ -c ./lib/window_functions.cpp -o ./build/window_functions.o 
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 #include <ncurses.h>
 #include "../include/window_functions.hpp"
-
+#include <iostream>
 
 int main(int argc, char** argv){
 
 	initscr(); 
-	//Create three divided windows
-	refresh(); 
-	getch(); 
+	cbreak();
+        noecho();
+	curs_set(0);	
+	int detailsBoxWidth = COLS; 
+	int detailsBoxHeight = LINES / 2; 
+	int detailsBoxStartX = 0; 
+	int detailsBoxStartY = detailsBoxHeight; 
+	int mapEditorBoxWidth = COLS/2; 
+	int mapEditorBoxHeight = LINES / 2;
+	int mapEditorBoxStartY = 0; 
+	int mapEditorBoxStartX = 0;	
+	int optionsBoxWidth = mapEditorBoxWidth; 
+	int optionsBoxHeight = mapEditorBoxHeight; 
+	int optionsBoxStartY = 0; 
+	int optionsBoxStartX = COLS / 2; 
+	WINDOW *detailsBoxWindow = create_new_window(detailsBoxHeight,detailsBoxWidth,detailsBoxStartY,detailsBoxStartX);
+	WINDOW *mapEditorBoxWindow = create_new_window(mapEditorBoxHeight,mapEditorBoxWidth,mapEditorBoxStartY,mapEditorBoxStartX);
+	WINDOW *optionsBoxWindow = create_new_window(optionsBoxHeight,optionsBoxWidth,optionsBoxStartY,optionsBoxStartX);
+	wgetch(detailsBoxWindow); 
 	endwin();
 	return 0; 
 
