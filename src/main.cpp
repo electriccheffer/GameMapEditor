895b290bb99ae79ad228670bf02ba17b9aa70ab2
electriccheffer <electriccheff@gmail.com>
2025-04-20 20:44:16 -0500

    APPLICATION TOGGLES BETWEEN WINDOWS

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@
 	int optionsBoxHeight;
 	int optionsBoxStartY;
 	int optionsBoxStartX;
+	int typedCharacter;
+	WINDOW *windows[3]; 
 
 void initializeWindowVariables(){
 
@@ -37,13 +39,46 @@ int main(int argc, char** argv){
 
 	initscr(); 
 	cbreak();
+	 
         noecho();
-	curs_set(0);	
+	curs_set(2);	
 	initializeWindowVariables(); 
 	WINDOW *detailsBoxWindow = create_new_window(detailsBoxHeight,detailsBoxWidth,detailsBoxStartY,detailsBoxStartX);
 	WINDOW *mapEditorBoxWindow = create_new_window(mapEditorBoxHeight,mapEditorBoxWidth,mapEditorBoxStartY,mapEditorBoxStartX);
 	WINDOW *optionsBoxWindow = create_new_window(optionsBoxHeight,optionsBoxWidth,optionsBoxStartY,optionsBoxStartX);
-	wgetch(detailsBoxWindow); 
+	
+	windows[0] = detailsBoxWindow; 
+	windows[1] = mapEditorBoxWindow; 
+	windows[2] = optionsBoxWindow; 
+
+	keypad(detailsBoxWindow,true);
+	keypad(mapEditorBoxWindow,true);
+	keypad(optionsBoxWindow,true);
+	wprintw(detailsBoxWindow,"Details Box Window"); 
+	wprintw(mapEditorBoxWindow,"Map Editor Box Window");
+	wprintw(optionsBoxWindow,"Options Box Window");
+	wrefresh(detailsBoxWindow);
+	wrefresh(mapEditorBoxWindow);
+	wrefresh(optionsBoxWindow); 	
+	WINDOW *activeWindow = mapEditorBoxWindow;
+	int windowIndex = 0; 	
+	while(((typedCharacter = wgetch(activeWindow)) != KEY_F(1))){
+		switch(typedCharacter){
+		
+			case KEY_PPAGE:
+				activeWindow = windows[++windowIndex % 3]; 
+				wmove(activeWindow,1,1); 
+				break; 
+			case KEY_NPAGE: 
+				activeWindow = windows[--windowIndex % 3]; 
+				wmove(activeWindow,1,1);
+				break; 
+		
+		}		
+		
+		wrefresh(activeWindow);
+	
+	}
 	endwin();
 	return 0; 
 
