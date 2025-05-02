8ee201aab519528139fbad8a02b40fadec3b9c31
electriccheffer <electriccheff@gmail.com>
2025-05-02 01:18:57 -0500

    stashing

--- src/objectEditorMain.cpp
+++ src/objectEditorMain.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv){
 	ObjectRenderModelFactory factory = {}; 
 	ObjectEditorRenderModel renderModel = factory.getModel(); 
 	ObjectEditorRenderView renderView(&renderModel); 
-	
+		
 	// make view 
 	// make controller
 	// make context 	
